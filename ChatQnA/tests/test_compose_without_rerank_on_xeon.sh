ae6289d57c015153a9b613ab565a97243d37da8b
CICD-at-OPEA <CICD@opea.dev>
2026-05-27 23:43:39 +0000

    Update vLLM version to v0.22.0rc2
    Signed-off-by: CICD-at-OPEA <CICD@opea.dev>

--- ChatQnA/tests/test_compose_without_rerank_on_xeon.sh
+++ ChatQnA/tests/test_compose_without_rerank_on_xeon.sh
@@ -25,7 +25,7 @@ function build_docker_images() {
     popd && sleep 1s
     git clone https://github.com/vllm-project/vllm.git && cd vllm
 
-    VLLM_VER=v0.21.1rc0
+    VLLM_VER=v0.22.0rc2
     echo "Check out vLLM tag ${VLLM_VER}"
     git checkout ${VLLM_VER} &> /dev/null
     # Not change the pwd
